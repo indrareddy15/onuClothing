238ac2beea9f2cf783ef7621df6e385b9f381d58
indrareddy15 <gantaindrareddy83@gmail.com>
2026-01-20 22:37:22 +0530

    Refactor code formatting for improved readability in authentication and database connection middleware

--- backend/Middelwares/authuser.js
+++ backend/Middelwares/authuser.js
@@ -1,23 +1,23 @@
 import jwt from 'jsonwebtoken'
 
-export const isAuthenticateuser = async(req, res, next)=>{
+export const isAuthenticateuser = async (req, res, next) => {
     const header = req.headers['authorization'];
-    if(!header){
+    if (!header) {
         console.log("No headers provided");
-        return res.status(403).json({success:false,message:"No headers provided"});
+        return res.status(403).json({ success: false, message: "No headers provided" });
     }
     const token = header && header.split(' ')[1];
     if (!token) {
         console.log("No token provided");
-        return res.status(403).json({success:false,message:"User token has been expired or not been generated"});
+        return res.status(403).json({ success: false, message: "User token has been expired or not been generated" });
     }
-    jwt.verify(token, process.env.SECRETID,(err, user) => {
-        if(err) {
-            console.error("Error Verification User: Log In Time Expired: "+ err.message);
+    jwt.verify(token, process.env.SECRETID, (err, user) => {
+        if (err) {
+            console.error("Error Verification User: Log In Time Expired: " + err.message);
             // return res.status(403).json({success:false, message:"Token is not valid"});
-            return res.status(401).json({success:false, message:`Log In Expired Please Refresh the Page to Login Again: ${err.message}`});
+            return res.status(401).json({ success: false, message: `Log In Expired Please Refresh the Page to Login Again: ${err.message}` });
         }
-		// console.log("Auth: User: ",user);
+        // console.log("Auth: User: ",user);
         req.user = user;
         next();
     });
--- backend/database/Database.js
+++ backend/database/Database.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose'
 
-const connectdatabse = async ()=>{
+const connectdatabse = async () => {
     try {
-        mongoose.set('strictQuery',false);
-        const data = await mongoose.connect(process.env.DB_URI,{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			serverSelectionTimeoutMS: 30000,  // Increase timeout limit (default is 30000 ms)
-		});
+        mongoose.set('strictQuery', false);
+        const data = await mongoose.connect(process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000,  // Increase timeout limit (default is 30000 ms)
+        });
         console.log(`Database connected ${data.connection.host}`)
     } catch (error) {
         console.error(`Database connection error: ${error.message}`);
