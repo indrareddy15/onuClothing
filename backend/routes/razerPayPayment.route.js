fec377077a06e479d1c84972d56a74357d9be0a9
indrareddy15 <gantaindrareddy83@gmail.com>
2026-01-30 15:22:15 +0530

    deleted unwanted files

--- backend/routes/razerPayPayment.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createOrder, OnPaymentCallBack, paymentVerification } from '../utility/razorPayGatewayHelper.js';
-import { isAuthenticateuser } from '../Middelwares/authuser.js';
-const route = express.Router();
-
-route.post("/order", isAuthenticateuser, createOrder);
-route.post("/paymentVerification", isAuthenticateuser, paymentVerification);
-route.post('/webhook/payments', OnPaymentCallBack)
-export default route;
